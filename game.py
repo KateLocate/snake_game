1f78724ba421e96af190e06b2281a7fe634b3c87
katelocate <katelocat3@gmail.com>
2023-07-19 20:51:01 +0200

    Add the initial state of snake.

--- game.py
+++ game.py
@@ -3,16 +3,29 @@
 # - operations for the snake to perform
 # - snake state
 # - visual representation of the field and the snake
+from enum import Enum, auto
+
+
+class SnakeDirection(Enum):
+    Up = auto()
+    Down = auto()
+    Left = auto()
+    Right = auto()
+
 
 class SnakeGame:
     FRUIT_CELL = ' @ '
-    FIELD_CELL = ' + '
-    SNAKE_BODY_PART = ' . '
+    FIELD_CELL = ' . '
+    SNAKE_BODY_PART = ' % '
 
     def __init__(self, field_size):
         self.field_size = field_size
-        self.fruit = None
         self.field = []
+
+        self.snake_body = []
+        self.snake_direction = []
+
+        self.fruit = None
 
     def generate_square_field(self):
         for i in range(self.field_size):
@@ -27,9 +40,10 @@ class SnakeGame:
 
     def generate_fruit_pos(self):
         import random
+        sample = [i for i in range(self.field_size)]
         self.fruit = {
-            'coordinate_x': random.choice([i for i in range(self.field_size)]),
-            'coordinate_y': random.choice([i for i in range(self.field_size)])
+            'coordinate_x': random.choice(sample),
+            'coordinate_y': random.choice(sample)
         }
 
     def add_fruit_to_the_field(self):
@@ -39,6 +53,25 @@ class SnakeGame:
             self.generate_square_field()
         self.field[self.fruit['coordinate_y']][self.fruit['coordinate_x']] = self.FRUIT_CELL
 
+    def generate_initial_snake_state(self):
+        self.snake_body = [{
+            'coordinate_x': int(self.field_size // 2),
+            'coordinate_y': int(self.field_size // 2)
+        }, {
+            'coordinate_x': int(self.field_size // 2) - 1,
+            'coordinate_y': int(self.field_size // 2)
+        }, {
+            'coordinate_x': int(self.field_size // 2) - 2,
+            'coordinate_y': int(self.field_size // 2)
+        }]
+        self.snake_direction = SnakeDirection.Right
+
+    def add_snake_to_the_field(self):
+        if not self.snake_body or not self.snake_direction:
+            self.generate_initial_snake_state()
+        for part in self.snake_body:
+            self.field[part['coordinate_y']][part['coordinate_x']] = self.SNAKE_BODY_PART
+
     def operate_snake_body(self):
         ...
 
@@ -46,5 +79,6 @@ class SnakeGame:
 if __name__ == '__main__':
     snake = SnakeGame(10)
     snake.add_fruit_to_the_field()
+    snake.add_snake_to_the_field()
     snake.draw_game_field()
 
